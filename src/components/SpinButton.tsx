6ba692ec9e53c0c8d616ce6c4817581824b43081
Suyoung Jung <devsuyoung@gmail.com>
2023-07-28 11:36:46 +0900

    refactor: HTML 언어 수정 및 접근성 일부 개선

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,55 +1,63 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
+import React, { useState, MouseEvent } from 'react';
+import './SpinButton.css';
 
 const SpinButton: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-  const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
+	const [count, setCount] = useState<number>(0);
+	const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
 
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+	const increment = () => {
+		setCount(prevCount => prevCount + 1);
+	};
 
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+	const decrement = () => {
+		setCount(prevCount => prevCount - 1);
+	};
 
-  const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
-    setIsTooltipVisible(!isTooltipVisible);
-  };
+	const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
+		setIsTooltipVisible(!isTooltipVisible);
+	};
 
-  return (
-    <section className="spinButtonContainer">
-      <div>
-        <h1>승객 선택</h1>
-        <div className="spinButtonLabel">
-          <label>성인</label>
-          <div
-            className="helpIcon"
-            onMouseEnter={toggleTooltip}
-            onMouseLeave={toggleTooltip}
-          >
-            ?
-            {isTooltipVisible && (
-              <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
-            )}
-          </div>
-        </div>
-        <button onClick={decrement} className="spinButton">
-          -
-        </button>
-        <input
-          type="text"
-          role="spinbutton"
-          readOnly
-          className="spinButtonInput"
-          value={count}
-        />
-        <button onClick={increment} className="spinButton">
-          +
-        </button>
-      </div>
-    </section>
-  );
+	return (
+		<section className="spinButtonContainer">
+			<div>
+				<h1>승객 선택</h1>
+				<div className="spinButtonLabel">
+					<label>성인</label>
+					<div
+						className="helpIcon"
+						onMouseEnter={toggleTooltip}
+						onMouseLeave={toggleTooltip}
+					>
+						?
+						{isTooltipVisible && (
+							<span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
+						)}
+					</div>
+				</div>
+				<button
+					onClick={decrement}
+					className="spinButton"
+					aria-label="탑승자 한 명 줄이기 버튼"
+				>
+					-
+				</button>
+				<input
+					type="number"
+					role="spinbutton"
+					readOnly
+					className="spinButtonInput"
+					value={count}
+				/>
+				<button
+					onClick={increment}
+					className="spinButton"
+					aria-label="탑승자 한 명 늘리기 버튼"
+				>
+					+
+				</button>
+			</div>
+		</section>
+	);
 };
 
 export default SpinButton;
